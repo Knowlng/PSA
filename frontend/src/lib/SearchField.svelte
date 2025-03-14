1828d65c464f45ca681bbf861b17761258e55e5b
Knowlng <adrijanas.maciunas@gmail.com>
2025-03-14 14:20:18 +0200

    - add SearchField component & integrate it into manageMovies page for actor and genre input

---
+++ frontend/src/lib/SearchField.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Input } from '@sveltestrap/sveltestrap';
+    import { createEventDispatcher } from 'svelte';
+    
+    export let placeholder = '';
+    export let value = '';
+
+    const dispatch = createEventDispatcher();
+    
+    function selectOnEnter(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            dispatch('enter', { value });
+            value = '';
+        }
+    }
+</script>
+
+<Input maxlength="50" type="text" bind:value={value} placeholder={placeholder} on:keydown={selectOnEnter}/>
--- frontend/src/routes/manageMovies/+page.svelte
+++ frontend/src/routes/manageMovies/+page.svelte
@@ -9,13 +9,20 @@
     Label,
     Container,
     Button
-  } from '@sveltestrap/sveltestrap';  
+  } from '@sveltestrap/sveltestrap'; 
+  import { ListGroup, ListGroupItem } from '@sveltestrap/sveltestrap';
+  import SearchField from '$lib/SearchField.svelte'; 
 
   let movieName = "";
   let ageRating = "";
   let movieDate = "";
   let gross = "";
   let description = "";
+  let actorName = '';
+  let genreName = '';
+
+  let genreArray = [];
+  let actorArray = [];
 
   let inner = '';
 
@@ -42,7 +49,9 @@
       ageRating,
       movieDate,
       gross,
-      description
+      description,
+      actors: actorArray,
+      genre: genreArray
     });
   }
 
@@ -57,58 +66,124 @@
       grossInvalid = false;
     }
   }
+
+  function handleActorEnter(event) {
+    const { value } = event.detail;
+    if (value) {
+      actorArray = [...actorArray, { name: value, role: "actor" }];
+    }
+  }
+
+
+  function preventEnterSubmit(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  }
+
+  function removeActor(index) {
+    actorArray = actorArray.filter((_, i) => i !== index);
+  }
+
+  function handleGenreEnter(event) {
+    const { value } = event.detail;
+    if (value) {
+      genreArray = [...genreArray, value];
+    }
+  }
+
+  function removeGenre(index) {
+    genreArray = genreArray.filter((_, i) => i !== index);
+  }
+
+
 </script>
 
 <Container>
     <h1>Edit Movie Info</h1>
-    <Form class="w-75 mx-auto" style="min-width: 450px; max-width: 700px;">
-      <FormGroup floating label="Move name" class="mb-4">
-        <Input 
-          bind:value={movieName} 
-          maxlength="255" 
-          required 
-          feedback="Can't be empty" 
-          invalid={movieNameInvalid}
-          on:focus={() => movieNameInvalid = false}
-        />
-      </FormGroup>
-      <InputGroup>
-        <FormGroup floating label="Age rating" class="override-mb">
-          <Input type="select" bind:value={ageRating}>
-            <option value="">Select</option>
-            <option value="G">G</option>
-            <option value="PG">PG</option>
-            <option value="PG-13">PG-13</option>
-            <option value="R">R</option>
-            <option value="NC-17">NC-17</option>
-          </Input>
+    <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
+    <div role="group" on:keydown={preventEnterSubmit}>
+      <Form class="w-75 mx-auto" style="min-width: 450px; max-width: 700px;">
+        <FormGroup floating label="Move name" class="mb-4">
+          <Input 
+            bind:value={movieName} 
+            maxlength="255" 
+            required 
+            feedback="Can't be empty" 
+            invalid={movieNameInvalid}
+            on:focus={() => movieNameInvalid = false}
+          />
         </FormGroup>
-        <Input type="date" bind:value={movieDate} />
-        <InputGroupText>$</InputGroupText>
-        <Input 
-        placeholder="Gross" 
-        type="number" step="1" min="0"
-        max="1000000000000" 
-        feedback="Invalid gross amount"
-        bind:value={gross} 
-        invalid={grossInvalid} 
-        on:focus={() => grossInvalid = false}/>
-      </InputGroup>
-      <FormGroup class="mt-2">
-        <Label>Description</Label>
-        <Input 
-          rows={1} type="textarea" bind:inner 
-          on:input={resize} bind:value={description}
-          maxlength="500" style="resize: none;" 
-          required feedback="Can't be empty"
-          invalid={descriptionInvalid}
-          on:focus={() => descriptionInvalid = false}
-        />
-      </FormGroup>
-      <Container class="text-center">
-        <Button type="submit"  color="primary" style="text-align:center;" on:click={submitHandler} >Submit</Button>
-      </Container>
-  </Form>
+        <InputGroup class="mb-4">
+          <FormGroup floating label="Age rating" class="override-mb">
+            <Input type="select" bind:value={ageRating}>
+              <option value="">Select</option>
+              <option value="G">G</option>
+              <option value="PG">PG</option>
+              <option value="PG-13">PG-13</option>
+              <option value="R">R</option>
+              <option value="NC-17">NC-17</option>
+            </Input>
+          </FormGroup>
+          <Input type="date" bind:value={movieDate} />
+          <InputGroupText>$</InputGroupText>
+          <Input 
+            placeholder="Gross" 
+            type="number" step="1" min="0"
+            max="1000000000000" 
+            feedback="Invalid gross amount"
+            bind:value={gross} 
+            invalid={grossInvalid} 
+            on:focus={() => grossInvalid = false}
+          />
+        </InputGroup>
+        <SearchField placeholder="Enter genre" bind:value={genreName} on:enter={handleGenreEnter}/>
+        <ListGroup flush class="mt-4">
+          {#each genreArray as genre, index}
+          <ListGroupItem tag="a" class="d-flex justify-content-between align-items-center pl-1">
+            <Container class="p-0 w-25">
+              {genre}
+            </Container>
+            <Container class="d-flex justify-content-end p-0">
+              <Button type="button" color="danger" on:click={() => removeGenre(index)}>Remove</Button>  
+            </Container>
+          </ListGroupItem>
+        {/each}
+        </ListGroup>
+        <SearchField placeholder="Enter Actor Name" bind:value={actorName} on:enter={handleActorEnter}/>
+        <ListGroup flush class="mt-4">
+          {#each actorArray as actor, index}
+            <ListGroupItem tag="a" class="d-flex justify-content-between align-items-center pl-1">
+              <Container class="p-0 w-25">
+                {actor.name}
+              </Container>
+              <Container class="d-flex justify-content-end p-0">
+                <Input type="select" class="w-25" bind:value={actorArray[index].role}>
+                  {#each ["director", "writer", "actor"] as option}
+                    <option value={option}>{option}</option>
+                  {/each}
+                </Input>
+                <Button type="button" color="danger" on:click={() => removeActor(index)}>Remove</Button>  
+              </Container>
+            </ListGroupItem>
+          {/each}
+        </ListGroup>
+        <FormGroup>
+          <Label>Description</Label>
+          <Input 
+            rows={1} type="textarea" bind:inner 
+            on:input={resize} bind:value={description}
+            maxlength="500" style="resize: none;" 
+            required feedback="Can't be empty"
+            invalid={descriptionInvalid}
+            on:focus={() => descriptionInvalid = false}
+          />
+        </FormGroup>
+        <Container class="text-center">
+          <Button type="submit"  color="primary" style="text-align:center;" on:click={submitHandler}>Submit</Button>
+        </Container>
+    </Form>
+  </div>
 </Container>
 
 <style>
@@ -125,6 +200,10 @@
   :global(input[type="number"]::-webkit-inner-spin-button) {
     -webkit-appearance: none;
     margin: 0;
+  }
+
+  :global(.list-group-item:last-child) {
+    margin-bottom: 2rem;
   }
 
 </style>
